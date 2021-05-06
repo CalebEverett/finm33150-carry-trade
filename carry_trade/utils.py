089e054498f0d82c52fdbd3f3f316223b3cee3e9
calebeverett <caleb@calebeverett.io>
2021-05-06 13:00:04 -0700

    charts done

--- carry_trade/utils.py
+++ carry_trade/utils.py
@@ -282,10 +282,12 @@ def run_strategy(
             )
         )
 
-    df_ret = pd.DataFrame(returns, columns=["date", "weekly_return"]).set_index("date")
+    df_ret = pd.DataFrame(returns, columns=["date", "per_return"]).set_index("date")
     df_profit = pd.DataFrame(
         profits, columns=["date", "lend", "fx_LB", "fx_BH", "interest", "total"]
     ).set_index("date")
+
+    df_ret.name = f"{fx_B},{yc_L}"
 
     return df_ret, df_profit
 
@@ -297,21 +299,12 @@ def run_strategy(
 COLORS = colors.qualitative.T10
 
 IS_labels = [
-    ("obs", lambda x: f"{x:>16d}"),
-    ("min:max", lambda x: f"{x[0]:>0.3f}:{x[1]:>0.3f}"),
-    ("mean", lambda x: f"{x:>13.4f}"),
-    ("std", lambda x: f"{x:>15.4f}"),
-    ("skewness", lambda x: f"{x:>11.4f}"),
-    ("kurtosis", lambda x: f"{x:>13.4f}"),
-]
-
-ET_labels = [
-    ("obs", lambda x: f"{x:>16d}"),
-    ("min:max", lambda x: f"{x[0]:>0.0f}:{x[1]:>0.0f}"),
-    ("mean", lambda x: f"{x:>13.2f}"),
-    ("std", lambda x: f"{x:>15.2f}"),
-    ("skewness", lambda x: f"{x:>11.4f}"),
-    ("kurtosis", lambda x: f"{x:>13.4f}"),
+    ("obs", lambda x: f"{x:>7d}"),
+    ("min:max", lambda x: f"{x[0]:>0.4f}:{x[1]:>0.3f}"),
+    ("mean", lambda x: f"{x:>7.4f}"),
+    ("std", lambda x: f"{x:>7.4f}"),
+    ("skewness", lambda x: f"{x:>7.4f}"),
+    ("kurtosis", lambda x: f"{x:>7.4f}"),
 ]
 
 
@@ -331,11 +324,13 @@ def get_moments_annotation(
     moments = list(stats.describe(s.to_numpy()))
     moments[3] = np.sqrt(moments[3])
 
+    sharpe = s.mean() / s.std()
+
     return go.layout.Annotation(
         text=(
-            f"{title}:<br>"
+            f"<b>sharpe: {sharpe:>8.4f}</b><br>"
             + ("<br>").join(
-                [f"{k[0]:<10}{k[1](moments[i])}" for i, k in enumerate(labels)]
+                [f"{k[0]:<9}{k[1](moments[i])}" for i, k in enumerate(labels)]
             )
         ),
         align="left",
@@ -345,10 +340,9 @@ def get_moments_annotation(
         x=x,
         y=y,
         bordercolor="black",
-        borderwidth=1,
+        borderwidth=0.5,
         borderpad=2,
         bgcolor="white",
-        font=dict(size=10),
         xanchor=xanchor,
         yanchor="top",
     )
@@ -364,16 +358,16 @@ def make_components_chart(
     dfs_yc: Dict,
     dfs_fx: Dict,
     dfs_libor: Dict,
-):
+) -> go.Figure:
 
     fig = make_subplots(
         rows=2,
         cols=2,
         subplot_titles=[
-            f"Change in 5-Year Yield: {yc_L}",
-            f"Change in FX Rate: {fx_B}:USD",
+            f"5-Year Yield: {yc_L}",
+            f"FX Rate: {fx_B}:USD",
             f"3 Month Libor: {libor}",
-            f"Change in FX Rate: {fx_L}:USD",
+            f"FX Rate: {fx_L}:USD",
         ],
         vertical_spacing=0.09,
         horizontal_spacing=0.08,
@@ -382,7 +376,7 @@ def make_components_chart(
     fig.add_trace(
         go.Scatter(
             x=date_range,
-            y=dfs_yc[yc_L].loc[date_range]["5-year"].pct_change(),
+            y=dfs_yc[yc_L].loc[date_range]["5-year"],
             line=dict(width=1, color=COLORS[0]),
             name=yc_L,
         ),
@@ -393,7 +387,7 @@ def make_components_chart(
     fig.add_trace(
         go.Scatter(
             x=date_range,
-            y=dfs_fx[fx_B].loc[date_range].rate.pct_change(),
+            y=dfs_fx[fx_B].loc[date_range].rate,
             line=dict(width=1, color=COLORS[0]),
             name=fx_B,
         ),
@@ -415,7 +409,7 @@ def make_components_chart(
     fig.add_trace(
         go.Scatter(
             x=date_range,
-            y=dfs_fx[fx_L].loc[date_range].loc[date_range].rate.pct_change(),
+            y=dfs_fx[fx_L].loc[date_range].loc[date_range].rate,
             line=dict(width=1, color=COLORS[0]),
             name=fx_L,
         ),
@@ -423,23 +417,173 @@ def make_components_chart(
         col=2,
     )
 
-    fig.update_xaxes(showline=True, linewidth=1, linecolor="black", mirror=True)
-    fig.update_yaxes(showline=True, linewidth=1, linecolor="black", mirror=True)
+    fig.update_xaxes(showline=True, linewidth=1, linecolor="grey", mirror=True)
+    fig.update_yaxes(showline=True, linewidth=1, linecolor="grey", mirror=True)
 
     fig.update_layout(
         title_text=(
-            f"Weekly Carry Trade: Borrow {fx_B} and Lend {fx_L}"
-            f"<br>{date_range.min().strftime('%Y-%m-%d')}"
+            f"Weekly Carry Trade: Borrow {fx_B}, Lend {yc_L}"
+            "<br>Underlying Securities: "
+            f"{date_range.min().strftime('%Y-%m-%d')}"
             f" - {date_range.max().strftime('%Y-%m-%d')}"
         ),
         showlegend=False,
         height=600,
         font=dict(size=10),
-        margin=dict(l=50, r=50, b=50, t=80),
+        margin=dict(l=50, r=50, b=40, t=90),
     )
 
     for i in fig["layout"]["annotations"]:
         i["font"]["size"] = 12
+
+    return fig
+
+
+def make_returns_chart(df_ret: pd.DataFrame) -> go.Figure:
+
+    fx_B, yc_L = df_ret.name.split(",")
+
+    fig = make_subplots(
+        rows=2,
+        cols=2,
+        subplot_titles=[
+            f"Weekly Returns",
+            f"Returns Distribution",
+            f"Cumulative Returns",
+            f"Q/Q Plot",
+        ],
+        vertical_spacing=0.09,
+        horizontal_spacing=0.08,
+    )
+
+    # Returns Distribution
+    returns = pd.cut(df_ret.per_return, 100).value_counts().sort_index()
+    midpoints = returns.index.map(lambda interval: interval.right).to_numpy()
+    norm_dist = stats.norm.pdf(
+        midpoints, loc=df_ret.per_return.mean(), scale=df_ret.per_return.std()
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=df_ret.index,
+            y=df_ret.per_return,
+            line=dict(width=1, color=COLORS[0]),
+            name="return",
+        ),
+        row=1,
+        col=1,
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=df_ret.index,
+            y=df_ret.per_return.cumsum(),
+            line=dict(width=1, color=COLORS[0]),
+            name="cum. return",
+        ),
+        row=2,
+        col=1,
+    )
+
+    fig.add_trace(
+        go.Bar(
+            x=[interval.mid for interval in returns.index],
+            y=returns / returns.sum() * 100,
+            name="pct. of returns",
+            marker=dict(color=COLORS[0]),
+        ),
+        row=1,
+        col=2,
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=[interval.mid for interval in returns.index],
+            y=norm_dist / norm_dist.sum() * 100,
+            name="normal",
+            line=dict(width=1, color=COLORS[1]),
+        ),
+        row=1,
+        col=2,
+    )
+
+    # Q/Q Data
+    returns_norm = (
+        (df_ret.per_return - df_ret.per_return.mean()) / df_ret.per_return.std()
+    ).sort_values()
+    norm_dist = pd.Series(
+        list(map(stats.norm.ppf, np.linspace(0.001, 0.999, len(df_ret.per_return)))),
+        name="normal",
+    )
+
+    fig.append_trace(
+        go.Scatter(
+            x=norm_dist,
+            y=returns_norm,
+            name="return norm.",
+            mode="markers",
+            marker=dict(color=COLORS[0], size=3),
+        ),
+        row=2,
+        col=2,
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=norm_dist,
+            y=norm_dist,
+            name="norm.",
+            line=dict(width=1, color=COLORS[1]),
+        ),
+        row=2,
+        col=2,
+    )
+
+    fig.add_annotation(
+        text=(f"{df_ret.per_return.cumsum()[-1]:0.4f}"),
+        xref="paper",
+        yref="y3",
+        x=0.465,
+        y=df_ret.per_return.cumsum()[-1],
+        xanchor="left",
+        showarrow=False,
+        align="left",
+    )
+
+    fig.add_annotation(
+        get_moments_annotation(
+            df_ret.per_return,
+            xref="paper",
+            yref="paper",
+            x=0.81,
+            y=0.23,
+            xanchor="left",
+            title="Returns",
+            labels=IS_labels,
+        ),
+        font=dict(size=6, family="Courier New, monospace"),
+    )
+
+    fig.update_xaxes(showline=True, linewidth=1, linecolor="black", mirror=True)
+    fig.update_yaxes(showline=True, linewidth=1, linecolor="black", mirror=True)
+
+    fig.update_layout(
+        title_text=(
+            f"Weekly Carry Trade: Borrow {fx_B}, Lend {yc_L}"
+            "<br>Returns: "
+            f"{df_ret.index.min().strftime('%Y-%m-%d')}"
+            f" - {df_ret.index.max().strftime('%Y-%m-%d')}"
+        ),
+        showlegend=False,
+        height=600,
+        font=dict(size=10),
+        margin=dict(l=50, r=50, b=50, t=100),
+    )
+
+    for i in fig["layout"]["annotations"]:
+        i["font"]["size"] = 12
+
+    fig.update_annotations(font=dict(size=10))
 
     return fig
 
